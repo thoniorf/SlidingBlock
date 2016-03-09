c058ec7ee683a73d5ce296745f970b219bff9050
thoniorf <thoniorf@gmail.com>
2016-03-09 17:47:55 +0100

    Updated MainFrame Added GamePanel

---
+++ src/it/slidingblock/gui/GamePanel.java
@@ -0,0 +1,34 @@
+package it.slidingblock.gui;
+
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import it.slidingblock.core.World;
+
+public class GamePanel extends JPanel
+{
+	private static final long serialVersionUID = 1L;
+	private JFrame frame;
+	private World world;
+
+	public GamePanel(MainFrame frame)
+	{
+		this.frame = frame;
+		this.world = new World();
+
+	}
+
+	public JFrame getFrame()
+	{
+		return this.frame;
+	}
+
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		world.paint(g);
+	}
+}
--- src/it/slidingblock/gui/MainFrame.java
+++ src/it/slidingblock/gui/MainFrame.java
@@ -1,27 +1,21 @@
 package it.slidingblock.gui;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
+
 public class MainFrame extends JFrame
 {
-	private static final long serialVersionUID=1L;
+	private static final long serialVersionUID = 1L;
 	private StartPanel startPanel;
+
 	public MainFrame()
 	{
-		this.setSize(1366,768);
-		this.setUndecorated(false);
+		this.setSize(1366, 768);
+		this.setUndecorated(true);
 		this.setLocationRelativeTo(null);
 		this.setTitle("Sliding Block");
 		this.setDefaultCloseOperation(0);
-		this.startPanel=new StartPanel(this);
-		this.addWindowListener(new WindowAdapter()
-		{
-			@Override
-			public void windowClosing(WindowEvent e)
-			{
-				System.exit(0);
-			}
-		});
+		this.startPanel = new StartPanel(this);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setContentPane(startPanel);
 		this.setVisible(true);
 	}
