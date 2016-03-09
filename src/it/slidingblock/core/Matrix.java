2c70699d83f2f0ba7fc55ca79520d13fc1b724ad
Giovanni <Giovanni@GB1609-pc>
2016-03-09 16:08:32 +0100

    Correct method sposta of Matrix Class

--- src/it/slidingblock/core/Matrix.java
+++ src/it/slidingblock/core/Matrix.java
@@ -1,99 +1,102 @@
 package it.slidingblock.core;
-
 import java.awt.Point;
 import java.util.ArrayList;
-
 import it.slidingblock.core.block.Block;
-
 public class Matrix
 {
 	private int width;
 	private int height;
 	private int[][] matrix;
-
-	public Matrix(int width, int height)
+	public Matrix(int width,int height)
 	{
-		this.width = width;
-		this.height = height;
-		this.matrix = new int[height][width];
-		for (int a = 0; a < height; a++)
-			for (int b = 0; b < width; b++)
-				matrix[a][b] = 0;
+		this.width=width;
+		this.height=height;
+		this.matrix=new int[height][width];
+		for (int a=0;a<height;a++)
+			for (int b=0;b<width;b++)
+				matrix[a][b]=0;
 	}
-
-	public boolean sposta(int id, Direction d)
+	public int getHeight()
 	{
-		// TODO da modificare
-		ArrayList<Point> temp = getPosition(id);
-		for (Point point : temp)
+		return height;
+	}
+	public int[][] getMatrix()
+	{
+		return matrix;
+	}
+	public ArrayList<Point> getPosition(int id)
+	{
+		ArrayList<Point> temp=new ArrayList<>();
+		for (int a=0;a<height;a++)
+			for (int b=0;b<width;b++)
+				if (matrix[a][b]==id)
+					temp.add(new Point(a,b));
+		return temp;
+	}
+	public int getWidth()
+	{
+		return width;
+	}
+	public boolean inserisci(Block b,Point o)
+	{
+		for (int i=o.y;i<o.y+b.getHeigth();i++)
 		{
-			matrix[(int) point.getX()][(int) point.getY()] = 0;
+			for (int j=o.x;j<o.x+b.getWidth();j++)
+			{
+				if (matrix[i][j]!=0)
+				{
+					return false;
+				}
+				matrix[i][j]=b.getId();
+			}
+		}
+		return true;
+	}
+	public boolean sposta(int id,Direction d)
+	{
+		ArrayList<Point> temp=getPosition(id);
+		for (Point point:temp)
+		{
+			matrix[(int) point.getX()][(int) point.getY()]=0;
 		}
 		switch (d)
 		{
 			case UP:
-				for (Point point : temp)
+				for (Point point:temp)
 				{
-					if (point.getY() > 0)
-						matrix[((int) point.getX()) - 1][(int) point.getY()] = 1;
+					if (point.getY()>0)
+						matrix[((int) point.getY())-1][(int) point.getX()]=id;
 					else
 						return false;
 				}
 				break;
 			case DOWN:
-				for (Point point : temp)
+				for (Point point:temp)
 				{
-					if (point.getX() < height)
-						matrix[((int) point.getX()) + 1][(int) point.getY()] = 1;
+					if (point.getX()<height)
+						matrix[((int) point.getY())+1][(int) point.getX()]=id;
 					else
 						return false;
 				}
 				break;
 			case RIGHT:
-				for (Point point : temp)
+				for (Point point:temp)
 				{
-					if (point.getX() < width)
-						matrix[(int) point.getX()][((int) point.getY()) + 1] = 1;
+					if (point.getX()<width)
+						matrix[(int) point.getY()][((int) point.getX())+1]=id;
 					else
 						return false;
 				}
 				break;
 			case LEFT:
-				for (Point point : temp)
+				for (Point point:temp)
 				{
-					if (point.getY() > 0)
-						matrix[(int) point.getX()][((int) point.getY()) - 1] = 1;
+					if (point.getY()>0)
+						matrix[(int) point.getY()][((int) point.getX())-1]=id;
 					else
 						return false;
 				}
 				break;
-		}
-		return true;
-	}
-
-	public ArrayList<Point> getPosition(int id)
-	{
-		ArrayList<Point> temp = new ArrayList<>();
-		for (int a = 0; a < height; a++)
-			for (int b = 0; b < width; b++)
-				if (matrix[a][b] == id)
-					temp.add(new Point(a, b));
-		return temp;
-	}
-
-	public boolean inserisci(Block b, Point o)
-	{
-		for (int i = o.y; i < o.y + b.getHeigth(); i++)
-		{
-			for (int j = o.x; j < o.x + b.getWidth(); j++)
-			{
-				if (matrix[i][j] != 0)
-				{
-					return false;
-				}
-				matrix[i][j] = b.getId();
-
-			}
 		}
 		return true;
 	}
