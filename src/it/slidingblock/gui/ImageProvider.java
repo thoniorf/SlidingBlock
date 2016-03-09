8d6916eff08aeab7ba4c800c1283862f63417d05
Giovanni <Giovanni@GB1609-pc>
2016-03-09 17:10:48 +0100

    Replaced blocks' images with hashMaps

--- src/it/slidingblock/gui/ImageProvider.java
+++ src/it/slidingblock/gui/ImageProvider.java
@@ -1,18 +1,12 @@
 package it.slidingblock.gui;
 import java.awt.Image;
 import java.io.IOException;
+import java.util.HashMap;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 public class ImageProvider
 {
-	private static Image redBlock1x1;
-	private static Image yellowBlock1x1;
-	private static Image redBlock2x1;
-	private static Image yellowBlock2x1;
-	private static Image redBlock1x2;
-	private static Image yellowBlock1x2;
-	private static Image redBlock2x2;
-	private static Image yellowBlock2x2;
+	private static HashMap<String,Image> blocks=new HashMap<>();
 	private static Image startPanel;
 	private static ImageIcon play1;
 	private static ImageIcon play2;
@@ -23,23 +17,39 @@ public class ImageProvider
 		try
 		{
 			startPanel=ImageIO.read(ImageProvider.class.getClassLoader().getResource("it/slidingblock/assets/startPanel.png"));
-			redBlock1x1=ImageIO.read(ImageProvider.class.getClassLoader().getResource("it/slidingblock/assets/redBlock1x1.png"));
-			redBlock1x2=ImageIO.read(ImageProvider.class.getClassLoader().getResource("it/slidingblock/assets/redBlock1x2.png"));
-			redBlock2x1=ImageIO.read(ImageProvider.class.getClassLoader().getResource("it/slidingblock/assets/redBlock2x1.png"));
-			redBlock2x2=ImageIO.read(ImageProvider.class.getClassLoader().getResource("it/slidingblock/assets/redBlock2x2.png"));
-			yellowBlock1x1=ImageIO.read(ImageProvider.class.getClassLoader().getResource("it/slidingblock/assets/yellowBlock1x1.png"));
-			yellowBlock1x2=ImageIO.read(ImageProvider.class.getClassLoader().getResource("it/slidingblock/assets/yellowBlock1x2.png"));
-			yellowBlock2x1=ImageIO.read(ImageProvider.class.getClassLoader().getResource("it/slidingblock/assets/yellowBlock2x1.png"));
-			yellowBlock2x2=ImageIO.read(ImageProvider.class.getClassLoader().getResource("it/slidingblock/assets/yellowBlock2x2.png"));
 			play1=new ImageIcon(ImageIO.read(ImageProvider.class.getClassLoader().getResource("it/slidingblock/assets/play1.png")));
 			play2=new ImageIcon(ImageIO.read(ImageProvider.class.getClassLoader().getResource("it/slidingblock/assets/play2.png")));
 			close1=new ImageIcon(ImageIO.read(ImageProvider.class.getClassLoader().getResource("it/slidingblock/assets/close1.png")));
 			close2=new ImageIcon(ImageIO.read(ImageProvider.class.getClassLoader().getResource("it/slidingblock/assets/close2.png")));
+			for (int a=1;a<3;a++)
+			{
+				for (int b=1;b<3;b++)
+				{
+					loadBlocks("redBlock"+a+"x"+b);
+					loadBlocks("yellowBlock"+a+"x"+b);
+				}
+			}
 		}
 		catch (final IOException e)
 		{
 			System.out.println("Cannot read Image");
 		}
+	}
+	private static void loadBlocks(String block) throws IOException
+	{
+		blocks.put(block,ImageIO.read(ImageProvider.class.getClassLoader().getResource("it/slidingblock/assets/"+block+".png")));
+	}
+	public static HashMap<String,Image> getBlocks()
+	{
+		return blocks;
+	}
+	public static ImageIcon getClose1()
+	{
+		return close1;
+	}
+	public static ImageIcon getClose2()
+	{
+		return close2;
 	}
 	public static ImageIcon getPlay1()
 	{
@@ -49,48 +59,8 @@ public class ImageProvider
 	{
 		return play2;
 	}
-	public static Image getRedBlock1x1()
-	{
-		return redBlock1x1;
-	}
-	public static Image getRedBlock1x2()
-	{
-		return redBlock1x2;
-	}
-	public static Image getRedBlock2x1()
-	{
-		return redBlock2x1;
-	}
-	public static Image getRedBlock2x2()
-	{
-		return redBlock2x2;
-	}
 	public static Image getStartPanel()
 	{
 		return startPanel;
-	}
-	public static Image getYellowBlock1x1()
-	{
-		return yellowBlock1x1;
-	}
-	public static Image getYellowBlock1x2()
-	{
-		return yellowBlock1x2;
-	}
-	public static Image getYellowBlock2x1()
-	{
-		return yellowBlock2x1;
-	}
-	public static Image getYellowBlock2x2()
-	{
-		return yellowBlock2x2;
-	}
-	public static ImageIcon getClose1()
-	{
-		return close1;
-	}
-	public static ImageIcon getClose2()
-	{
-		return close2;
 	}
 }
