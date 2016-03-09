bf0229465e9a852d843f590b58000d361c784fbe
Giovanni <Giovanni@GB1609-pc>
2016-03-09 18:05:30 +0100

    Modified MainFrame

--- src/it/slidingblock/gui/MainFrame.java
+++ src/it/slidingblock/gui/MainFrame.java
@@ -1,22 +1,21 @@
 package it.slidingblock.gui;
-
 import javax.swing.JFrame;
-
 public class MainFrame extends JFrame
 {
-	private static final long serialVersionUID = 1L;
-	private StartPanel startPanel;
-
+	private static final long serialVersionUID=1L;
 	public MainFrame()
 	{
-		this.setSize(1366, 768);
-		this.setUndecorated(true);
+		this.setSize(1366,768);
+		this.setUndecorated(false);
 		this.setLocationRelativeTo(null);
 		this.setTitle("Sliding Block");
-		this.setDefaultCloseOperation(0);
-		this.startPanel = new StartPanel(this);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setContentPane(startPanel);
+		this.setContentPane(new StartPanel(this));
 		this.setVisible(true);
+	}
+	public void switchPanelPlay()
+	{
+		this.setContentPane(new GamePanel(this));
+		this.revalidate();
 	}
 }
